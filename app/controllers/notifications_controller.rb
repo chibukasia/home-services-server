f5a55891143ed76ff9cb6a371ed0584969228a57
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-01-11 22:29:20 +0300

    added CRUD methods

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,45 @@
 class NotificationsController < ApplicationController
+    before_action :authorize
+    
+    rescue_from ActiveRecord::RecordNotFound, with: :notification_not_found
+    def index 
+        notifications = Notification.all 
+        render json: notifications 
+    end 
+
+    def show 
+        notification = find_notification
+        render json: notification, status: :ok 
+    end
+
+    def create 
+        notification = Notification.create!(notification_params)
+        render json: notification, status: :created
+    end
+
+    def update 
+        notification = find_notification
+        notification.update!(notification_params)
+        render json: notification, status: :accepted
+    end
+
+    # Private methods 
+    private 
+
+    # find notification
+    def find_notification
+        Notification.find(params[:id])
+    end
+
+    def notification_params
+        params.permit(:message, :user_id)
+    end
+
+    def notification_not_found
+        render json: {error: "Notification not found"}, status: :not_found
+    end 
+
+    def authorize
+        render json: {error: "Login or signup to continue"}, status: :unauthorized unless session.include? :user_id
+    end
 end
