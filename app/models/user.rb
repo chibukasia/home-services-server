dc592309c0c3bc8e471696331b64aa5369273daa
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-01-11 22:08:05 +0300

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,16 @@ class User < ApplicationRecord
     has_many :notifications, dependent: :destroy
     has_one :profile, dependent: :destroy
     has_many :reviews, dependent: :destroy
-    has_many :user_services, through: :reviews, dependent: :destroy
+    has_many :user_services, through: :reviews
     has_many :unlocks, dependent: :destroy
     has_many :user_contracts, dependent: :destroy
+
+    # Validations
+    validates :email, uniqueness: true, presence: true 
+    validates :username, uniqueness: true, length:{minimum: 3, maximum: 15}, presence: true
+    validates :phone, uniqueness: true, presence: true, {minimum: 9, maximum: 13} 
+    validates :first_name, presence: true 
+    validates :last_name, presence: true
+    # validates :role, presence: true
+    
 end
