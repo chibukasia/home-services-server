60b450d495d17d669ba6c31f2d666139cd45a02d
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-01-30 22:59:42 +0300

    added crud methods on terms controller

--- app/controllers/terms_and_conditions_controller.rb
+++ app/controllers/terms_and_conditions_controller.rb
@@ -1,2 +1,39 @@
 class TermsAndConditionsController < ApplicationController
+
+    def index 
+        terms = TermsAndCondition.all 
+        render json: terms
+    end
+
+    def create
+        term = TermsAndCondition.create!(term_params)
+        render json: term, status: :created 
+    end
+
+    def update
+        term = find_term
+        term.update!(term_params)
+        render json: term, status: :accepted
+    end
+
+    def destroy
+        term = find_term
+        term.destroy
+        head :no_content
+    end
+
+    # Private methods 
+    private 
+
+    def term_params
+        params.permit(:description)
+    end
+
+    def find_term
+        TermsAndCondition.find(params[:id])
+    end
+
+    def term_not_found
+        render json:{error: "Term and condition not found"}, status: :not_found
+    end
 end
