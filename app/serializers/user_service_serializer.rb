2e0d33d3bb413d27de79f4757ee8a986fde32dcd
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-01 22:01:59 +0300

    edited user service serializer

--- app/serializers/user_service_serializer.rb
+++ app/serializers/user_service_serializer.rb
@@ -1,3 +1,5 @@
 class UserServiceSerializer < ActiveModel::Serializer
   attributes :id, :description, :quotation, :location, :created_at, :updated_at, :start_time, :end_time
+  belongs_to :service
+  belongs_to :user
 end
