2ea2278a5206d5041a77fb21fc8cba67d4b1c982
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-01 16:36:34 +0300

    updated terms and condition controller

--- app/controllers/terms_and_conditions_controller.rb
+++ app/controllers/terms_and_conditions_controller.rb
@@ -1,5 +1,5 @@
 class TermsAndConditionsController < ApplicationController
-
+    skip_before_action :authorize, only: [:index]
     def index 
         terms = TermsAndCondition.all 
         render json: terms
