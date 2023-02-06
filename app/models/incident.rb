0aeb298b03496358dbf61679c068a7a21ce9cfbb
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-06 23:12:31 +0300

    added file attachments

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,9 +1,11 @@
 class Incident < ApplicationRecord
     # Associations
     belongs_to :appointment_order
+    has_one_attached :evidence
 
     # Validations
     validates :incident_name, presence: true
     validates :incident_description, presence: true, length: {minimum: 100}
     validates :incident_location, presence: true
+    validates :evidence, presence: true
 end
--- app/models/user_contract.rb
+++ app/models/user_contract.rb
@@ -1,9 +1,12 @@
 class UserContract < ApplicationRecord
     belongs_to :user
+    has_one_attached :id_copy 
+
 
     # Validations 
     validates :title, presence: true 
     validates :description, presence: true, length: {minimum: 100}
     validates :monthly_pay, presence: true, comparison: {greater_than_or_equal_to: 3000}
     validates :period, presence: true
+    validates :id_copy, presence: true
 end
