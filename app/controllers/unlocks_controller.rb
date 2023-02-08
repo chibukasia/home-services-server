4f5bbccf652fd36932ebae7b3150ed7d7749021b
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-08 22:45:46 +0300

    added crud methods

--- app/controllers/unlocks_controller.rb
+++ app/controllers/unlocks_controller.rb
@@ -1,2 +1,22 @@
 class UnlocksController < ApplicationController
+
+    def create
+        unlocks = current_user.unlocks.create!(unlock_params) 
+        render json: unlocks, status: :created
+    end
+
+    def update
+        unlock = Unlock.find(params[:id])
+        if unlock
+            unlock.update!(unlock_params)
+            render json: unlock, status: :accepted
+        else 
+            render json: {error: "unlock not found"}, status: :not_found
+        end
+    end
+
+    private 
+    def unlock_params
+        params.permit(:used_unlocks, :remaining_unlocks, :total_unlocks, :current_unlocks, :user_id)
+    end
 end
