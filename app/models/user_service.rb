b1dd273ebb05e893f972634b1e8c0366c006352b
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-09 23:49:01 +0300

    added custom validation

--- app/models/user_service.rb
+++ app/models/user_service.rb
@@ -12,10 +12,17 @@ class UserService < ApplicationRecord
     validates :location, presence: true
     validates :quotation, presence: true, comparison: {greater_than_or_equal_to: 50}
     validates :images, presence: true 
+    validate :must_have_at_least_three_images 
 
     # def image_url
     #     Rails.application.routes.url_helpers.url_for(image) if image.attached?
     # end
+
+    def must_have_at_least_three_images
+        unless  images.length >= 3
+            errors.add(:images, "At least three images are needed")
+        end
+    end
 
     def image_urls
         images.map do |image|  
