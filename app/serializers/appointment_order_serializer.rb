5d76f65e166dd39a87838f399592b3a21b0a42be
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-11 00:29:05 +0300

    modified serializers

--- app/serializers/appointment_order_serializer.rb
+++ app/serializers/appointment_order_serializer.rb
@@ -1,4 +1,6 @@
 class AppointmentOrderSerializer < ActiveModel::Serializer
   attributes :id, :appointment_date, :status, :created_at, :updated_at
   belongs_to :user_service
+  belongs_to :user
+  has_many :incidents
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :email, :phone, :password_digest, :role, :on_probation, :active, :address, :premium
+  attributes :id, :first_name, :last_name, :username, :email, :phone, :role, :on_probation, :active, :address, :premium
 end
