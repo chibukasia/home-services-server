6553077a90ef0fddfb0c9bd07d604151661cd7d6
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-11 22:50:58 +0300

    modified serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :email, :phone, :role, :on_probation, :active, :address, :premium
+  attributes :id, :first_name, :last_name, :username, :email, :phone, :role, :on_probation, :active, :address, :premium, :full_name 
+  has_one :profile
+  def full_name
+    "#{self.object.first_name.capitalize} #{self.object.last_name.capitalize}"
+  end
 end
--- app/serializers/user_service_serializer.rb
+++ app/serializers/user_service_serializer.rb
@@ -1,5 +1,5 @@
 class UserServiceSerializer < ActiveModel::Serializer
-  attributes :id, :description, :quotation, :location, :created_at, :updated_at, :start_time, :end_time, :image_urls
+  attributes :id, :description, :quotation, :location, :created_at, :updated_at, :start_time, :end_time, :image_urls, :user_id, :service_id
   belongs_to :service
   belongs_to :user
 end
